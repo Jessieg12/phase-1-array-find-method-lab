bfa63f17195f98a3f0f2884d0fc432bec64655a0
Jessie Guzman <jessieguzman777@gmail.com>
2022-11-07 22:15:04 -0600

    commit tree b749266ffe884218def209df53724537c3701d3f

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code your solution here
+// function superbowlWin(record){
+// const findyear = record.find(yearWon)
+// return findyear;
+// }
+
+// function yearWon(record){
+//    if(record.result === "W"){
+//    return record.year;
+//    } else {
+//     return undefined;
+//    }
+// }
+
+// function superbowlWin(record){
+//   const dub = record.find(dub.result === "W")
+//     if(dub.result === "W"){
+//       return dub.year
+//     }else {
+//       return 'no win'
+//     }
+// }
+
+function superbowlWin(record){
+  const result = record.find(record => record.result === "W")
+  return result ? result.year : undefined
+}
